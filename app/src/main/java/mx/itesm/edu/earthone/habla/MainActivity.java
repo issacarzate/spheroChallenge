4b69b6bd453bd7ba1f6c169d718778c3e9405960
Issac Arzate Soto <issac@MacBook-Pro-de-Issac.local>
2018-03-20 14:39:20 -0600

    first commit

--- app/src/main/java/mx/itesm/edu/earthone/habla/MainActivity.java
+++ app/src/main/java/mx/itesm/edu/earthone/habla/MainActivity.java
@@ -1,20 +1,43 @@
 package mx.itesm.edu.earthone.habla;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.orbotix.ConvenienceRobot;
+import com.orbotix.DualStackDiscoveryAgent;
+import com.orbotix.common.DiscoveryException;
+import com.orbotix.common.ResponseListener;
+import com.orbotix.common.Robot;
+import com.orbotix.common.RobotChangedStateListener;
+import com.orbotix.common.internal.AsyncMessage;
+import com.orbotix.common.internal.DeviceResponse;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends Activity implements TextToSpeech.OnInitListener{
+public class MainActivity extends Activity implements TextToSpeech.OnInitListener, RobotChangedStateListener, ResponseListener {
+
+    private Button go, stop, back;
+
+    private final int REQUEST_PERMISSION = 42;    //Pedir permiso
+    private float ROBOT_SPEED = 6.0f;   //Velocidad de robot
+
+    private int direction;
+
+    private ConvenienceRobot convenienceRobot;
 
     private TextToSpeech textToSpeech = null;
     private final int CHECK_TTS = 1000;
@@ -58,6 +81,19 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         Intent ttsIntent = new Intent();
         ttsIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
         startActivityForResult(ttsIntent, CHECK_TTS);
+
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener(this);
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+            int hasLocationPermission = checkSelfPermission( Manifest.permission.ACCESS_COARSE_LOCATION );
+            if( hasLocationPermission != PackageManager.PERMISSION_GRANTED ) {
+                Log.e( "Sphero", "Location permission has not already been granted" );
+                List<String> permissions = new ArrayList<String>();
+                permissions.add( Manifest.permission.ACCESS_COARSE_LOCATION);
+                requestPermissions(permissions.toArray(new String[permissions.size()] ), REQUEST_PERMISSION );
+            } else {
+                Log.d( "Sphero", "Location permission already granted" );
+            }
+        }
     }
 
     @Override
@@ -77,6 +113,35 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
                 if(resultCode == RESULT_OK && data != null){
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     textView.setText(result.get(0));
+                    if(result.get(0).equals("go")){
+                        if(convenienceRobot == null){
+                            Toast.makeText(MainActivity.this, "Conectate a un sphero", Toast.LENGTH_SHORT).show();
+                        }else {
+                            convenienceRobot.setLed(.5f, .5f, 0f);
+                            direction = 180;
+                            convenienceRobot.drive(direction, ROBOT_SPEED);
+                        }
+                    }
+                    if(result.get(0).equals("back")){
+                        if(convenienceRobot == null){
+                            Toast.makeText(MainActivity.this, "Conectate a un sphero", Toast.LENGTH_SHORT).show();
+                        }else {
+                            convenienceRobot.setLed(.5f, .5f, 0f);
+                            direction = 0;
+                            convenienceRobot.drive(direction, ROBOT_SPEED);
+                        }
+                    }
+                    if(result.get(0).equals("stop")){
+                        if(convenienceRobot == null){
+                            Toast.makeText(MainActivity.this, "Conectate a un sphero", Toast.LENGTH_SHORT).show();
+                        }else {
+                            convenienceRobot.setLed(1f, 0f, 0f);
+                            convenienceRobot.stop();
+                        }
+                    }
+                    if(!result.get(0).equals("go") || !result.get(0).equals("back") || !result.get(0).equals("stop")){
+                        Toast.makeText(MainActivity.this, "Comando no reconocido", Toast.LENGTH_SHORT).show();
+                    }
                 }
 
                 break;
@@ -121,4 +186,79 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
             textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 
+    @Override
+    public void handleResponse(DeviceResponse deviceResponse, Robot robot) {
+
+    }
+
+    @Override
+    public void handleStringResponse(String s, Robot robot) {
+
+    }
+
+    @Override
+    public void handleAsyncMessage(AsyncMessage asyncMessage, Robot robot) {
+
+    }
+
+    @Override
+    public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType robotChangedStateNotificationType) {
+        switch (robotChangedStateNotificationType){
+            case Online:
+                convenienceRobot = new ConvenienceRobot(robot);
+                convenienceRobot.addResponseListener(this);
+                convenienceRobot.enableCollisions(true);
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch ( requestCode ) {
+            case REQUEST_PERMISSION: {
+                for( int i = 0; i < permissions.length; i++ ) {
+                    if( grantResults[i] == PackageManager.PERMISSION_GRANTED ) {
+                        startDiscovery();
+                        Log.d( "Permissions", "Permission Granted: " + permissions[i] );
+                    } else if( grantResults[i] == PackageManager.PERMISSION_DENIED ) {
+                        Log.d( "Permissions", "Permission Denied: " + permissions[i] );
+                    }
+                }
+            }
+            break;
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startDiscovery();
+
+    }
+
+    private void startDiscovery() {
+        if( !DualStackDiscoveryAgent.getInstance().isDiscovering() ) {
+            try {
+                DualStackDiscoveryAgent.getInstance().startDiscovery( this );
+            } catch (DiscoveryException e) {
+                Log.e("Sphero", "DiscoveryException: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if( DualStackDiscoveryAgent.getInstance().isDiscovering() ) {
+            DualStackDiscoveryAgent.getInstance().stopDiscovery();
+        }
+        if( convenienceRobot != null ) {
+            convenienceRobot.disconnect();
+            convenienceRobot = null;
+        }
+
+        super.onStop();
+    }
 }
